b012d947ee800e8c5f67e6982fbb43fd6b2eff46
SzymonMrozek <szymon.mrozek.sm@gmail.com>
2018-01-11 10:07:55 +0100

    Added initial protocols, fields, functions for LoadableState

--- Stefan.playground/Contents.swift
+++ Stefan.playground/Contents.swift
@@ -5,18 +5,119 @@ import Foundation
 /////////////////////
 
 public protocol LoadableStateType {
+    
     associatedtype ItemType
 }
 
-public enum LoadableState<T> {
-    typealias ItemType = T
+public protocol PlainLoadableStateType: LoadableStateType {
+    
+    var itemsCount: Int { get }
+    var items: [ItemType] { get }
+}
+public protocol SectionatedLoadableStateType: LoadableStateType {
+    
+    var sectionsCount: Int { get }
+    var items: [[ItemType]] { get }
+    func itemsCount(inSection section: Int) -> Int
+    func items(inSection section: Int) -> [ItemType]
+}
+
+public enum PlainLoadableState<T>: PlainLoadableStateType {
+    
+    public typealias ItemType = T
     
     case idle
     case loading
-    case loaded(items: [T])
-    case refreshing
     case noContent
+    case loaded(items: [ItemType])
+    case refreshing(silent: Bool)
     case error(Error)
+    
+    public init(_ items: [ItemType]) {
+        if items.count > 0 {
+            self = .loaded(items: items)
+        } else {
+            self = .noContent
+        }
+    }
+    
+    public var itemsCount: Int {
+        switch self {
+        case let .loaded(items):
+            return items.count
+        default:
+            return 0
+        }
+    }
+    
+    public var items: [ItemType] {
+        switch self {
+        case let .loaded(items):
+            return items
+        default:
+            return []
+        }
+    }
+}
+
+
+public enum SectionatedLoadableState<T>: SectionatedLoadableStateType {
+    
+    public typealias ItemType = T
+    
+    case idle
+    case loading
+    case noContent
+    case loaded(items: [[ItemType]])
+    case refreshing(silent: Bool)
+    case error(Error)
+    
+    public init(_ items: [[ItemType]]) {
+        if items.count > 0 {
+            self = .loaded(items: items)
+        } else {
+            self = .noContent
+        }
+    }
+    
+    public var sectionsCount: Int {
+        switch self {
+        case let .loaded(items):
+            return items.count
+        default:
+            return 0
+        }
+    }
+    
+    public var items: [[ItemType]] {
+        switch self {
+        case let .loaded(items):
+            return items
+        default:
+            return [[]]
+        }
+    }
+    
+    public func itemsCount(inSection section: Int) -> Int {
+        switch self {
+        case let .loaded(items):
+            guard self.sectionsCount > section else { return 0 }
+            return items[section].count
+        default:
+            return 0
+        }
+    }
+    
+    public func items(inSection section: Int) -> [ItemType] {
+        switch self {
+        case let .loaded(items):
+            guard self.sectionsCount > section else { return [] }
+            return items[section]
+        default:
+            return []
+        }
+    }
+    
 }
 
 // Extension do arraya na loadable state
